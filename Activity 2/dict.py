7e2378152f0fdfb32e1255c7591f8c217d282f05
aakritiisharmaa <81676701+aakritiisharmaa@users.noreply.github.com>
2022-09-25 22:05:27 +0530

    Activity 2

---
+++ Activity 2/dict.py
@@ -0,0 +1,9 @@
+d ={1:10,2:20,3:30,4:40,5:50,6:60}
+def is_key_present(x):
+    if x in d:
+        print('key is present in the dict.')
+    else:
+        print('key is not present in the dict.')
+is_key_present(5)
+is_key_present(9)
+    
